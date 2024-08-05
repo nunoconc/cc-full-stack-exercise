12e0b2710b5adee14ab32c5804be5659582c2114
Nuno Conceicao <26148760+nunoconc@users.noreply.github.com>
2024-08-05 15:40:39 +0100

    chore: setting up initial backend project

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+// server.mjs
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, 'localhost', () => {
+  console.log('Listening on localhost:3000');
+});
+
+// run with `node server.mjs`
