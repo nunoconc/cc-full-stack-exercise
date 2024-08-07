5e6502b14fc9254c6366fe4a1e985883fe992106
Nuno Conceicao <26148760+nunoconc@users.noreply.github.com>
2024-08-07 22:01:25 +0100

    chore: configure husky and pre commit hook validations

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
+    root: true,
     parser: '@typescript-eslint/parser',
     env: {
         node: true,
-        browser: true,
         es2021: true,
-        jest: true,
     },
     extends: [
         'eslint:recommended',
@@ -14,7 +13,6 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        jsx: true, // Enable JSX support
     },
     plugins: ['@typescript-eslint'],
     rules: {
