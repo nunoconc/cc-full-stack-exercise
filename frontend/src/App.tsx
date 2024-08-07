e8e0f2d1e1123c256b85681c7cce10a0a964095b
Nuno Conceicao <26148760+nunoconc@users.noreply.github.com>
2024-08-07 14:06:29 +0100

    feat: header mui css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Router from './components/router';
+import { Link, Typography } from '@mui/material';
+import './index.css';
+
 
 function App() {
   return (
     <div>
-      <header>
-        <h1 onClick={()=> window.location.href = '/'}>{process.env.REACT_APP_TITLE}</h1>
+      <header className='header'>
+        <Link href={"/"}>
+          <Typography variant="h1">Securities</Typography>
+        </Link>
       </header>
       <main>
         <Router />
