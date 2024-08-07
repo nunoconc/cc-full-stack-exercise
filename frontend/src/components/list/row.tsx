ea258ff88f004f68e26f8f4b19d3d7d98906479b
Nuno Conceicao <26148760+nunoconc@users.noreply.github.com>
2024-08-07 14:25:31 +0100

    feat: list css

--- frontend/src/components/list/row.tsx
+++ frontend/src/components/list/row.tsx
@@ -9,19 +9,18 @@ interface IRow {
 
 export default function Row({ company }: IRow): JSX.Element {
     const navigate = useNavigate();
-    const color = company.trend > 0.20 ? 'blue' : company.trend > -0.20 ? 'green' : 'red';
+    const color =
+        company.trend > 0.2 ? 'blue' : company.trend > -0.2 ? 'green' : 'red';
 
     return (
-        <>
-            <TableRow onClick={() => navigate(`/securities/${company.ticker}`)}>
-                <TableCell>{company.ticker}</TableCell>
-                <TableCell>{company.securityName}</TableCell>
-                <TableCell>{company.sector}</TableCell>
-                <TableCell>{company.country}</TableCell>
-                <TableCell className={`trendCell ${color}`}>
-                    <Typography color={'whitesmoke'}>{company.trend}</Typography>
-                </TableCell>
-            </TableRow>
-        </>
+        <TableRow className='tableRow' onClick={() => navigate(`/securities/${company.ticker}`)}>
+            <TableCell>{company.ticker}</TableCell>
+            <TableCell>{company.securityName}</TableCell>
+            <TableCell>{company.sector}</TableCell>
+            <TableCell>{company.country}</TableCell>
+            <TableCell className={`trendCell ${color}`}>
+                <Typography color={'white'}>{company.trend}</Typography>
+            </TableCell>
+        </TableRow>
     );
 }
