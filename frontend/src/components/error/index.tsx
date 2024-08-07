88ef1c31aa22d4cfe423992d96af562e89d85821
Nuno Conceicao <26148760+nunoconc@users.noreply.github.com>
2024-08-07 14:53:47 +0100

    feat: error page css mui

--- frontend/src/components/error/index.tsx
+++ frontend/src/components/error/index.tsx
@@ -1,9 +1,20 @@
 import { JSX } from 'react';
+import { Typography } from '@mui/material';
+import { useRouteError } from 'react-router-dom';
+import './index.css';
+
 
 export default function Error(): JSX.Element {
+    const error = useRouteError() as Error;
+    
     return (
-        <>
-            <p>Error Page</p>
-        </>
+        <div className="errorContainer">
+            <Typography variant="h4">
+                <p>Ups! Some error has occurred while loading..</p>
+            </Typography>
+            <Typography variant="h6">
+                <b>{error?.name}</b> - {error?.message}
+            </Typography>
+        </div>
     );
 }
